9c207932f99779f7903c111685846a25d046cda8
TCHATEU NANGKAMG Marc Loic (INTERN) <MTG7@dsone.3ds.com>
2024-08-22 15:35:10 +0200

    Workging on Building Chat Functionality

--- AI_chat_bot/ai-chat-bot/src/App.jsx
+++ AI_chat_bot/ai-chat-bot/src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ChatBotStart from './Components/ChatBotStart'
+import ChatBotApp from './Components/ChatBotApp'
 
 const App = () => {
+
+  const [isChatting, setIsChatting] = useState(false)
+  const [chats, setChats] = useState([])
+
+  const handleStartChat = () => {
+    setIsChatting(true)
+    if(chats.length === 0) {
+      const newChat = {
+        id: `Chat ${new Date().toLocaleDateString("en-GB")} ${Date().toLocaleTimeString()}`
+      }
+    }
+  }
+
+  const handleGoBack = () => {
+    setIsChatting(false)
+  }
+
   return (
     <div className='container'>
-        <ChatBotStart />
+      {isChatting ? 
+        (<ChatBotApp onGoBack={handleGoBack} />) : 
+        (<ChatBotStart onStartChat={handleStartChat} />)
+      }
     </div>
   )
 }
